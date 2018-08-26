0f79996831176da53023a144d985442268921b25
bustatech <bustatech@Bustatech>
2018-08-26 19:34:00 -0400

    proyecto inicial

---
+++ Colegio/src/entidades/Alumnos.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author bustatech
+ */
+public class Alumnos {
+    private Integer idAlumno;
+    private String nombre;
+    private String apellido;
+    private Integer cedula;
+
+    public Integer getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(Integer idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(Integer cedula) {
+        this.cedula = cedula;
+    }
+    
+    
+            
+    
+}
